bfab824d5ac2dae67c74943f9c6d6745d85de0f2
Nyamekye Effah-Marfo <44102986+jailbreak-0@users.noreply.github.com>
2025-05-12 19:26:16 +0000

    Update LostItemCard.tsx

--- src/components/LostItemCard.tsx
+++ src/components/LostItemCard.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 export default function LostItemCard({ item }: Props) {
   return (
-    <div className="border rounded-lg p-4 shadow bg-white dark:bg-gray-800">
+    <div className="border w-md rounded-lg p-4 shadow bg-white dark:bg-gray-800">
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-semibold">{item.title}</h2>
         <span
@@ -27,17 +27,20 @@ export default function LostItemCard({ item }: Props) {
         </span>
       </div>
       <p className="text-gray-600 dark:text-gray-300 mt-2">{item.description}</p>
-      {item.is_claimed && (
-        <span className="text-xs text-yellow-600 font-medium">
-          ✅ Claimed
-        </span>
-      )}
-      <Link
-        href={`/lost-and-found/${item.id}`}
-        className="inline-block mt-3 text-blue-600 hover:underline text-sm"
-      >
-        View Details
-      </Link>
+      
+      <div className="flex flex-col justify-start max-w-30 mt-4">
+        {item.is_claimed && (
+          <span className="text-sm text-yellow-600 font-medium gap-2">
+            ✅ Claimed
+          </span>
+        )}
+        <Link
+          href={`/lost-and-found/${item.id}`}
+          className="inline-block text-center mt-3 hover:text-blue-600 hover:underline text-sm bg-[#22AB39] text-white py-2 px-4 rounded"
+        >
+          View Details
+        </Link>
+      </div>
     </div>
   );
 }
